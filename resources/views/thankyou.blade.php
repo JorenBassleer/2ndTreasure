ca1770238e146e133e42bc53360dd00115d2124b
Badassleer <32762996+Badassleer@users.noreply.github.com>
2020-08-31 09:00:58 +0200

    thank you update

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -9,29 +9,27 @@
             </div>
             <div class="card">
                 <div class="card-body">
-                    @guest
                     <div class="text-center">
-                        You can collect your {{$goodiebag->treasures}} treasure(s) here:
-                    </div>
-                    @endguest
-                    <div class="text-center">
-                        <h3>You stopped around {{$goodiebag->total_kg}}kg to going to waste. Good job!</h3>
+                        <h3>You stopped around {{presentWeightToKg($goodiebag->total_kg, true)}} to going to waste. Good job!</h3>
                     </div>
                     
                     <div class="text-center">
                         <div class="fb-share-button" data-href="https://2ndtreasure.1819.joren.bassleer.nxtmediatech.eu/" data-layout="button" data-size="small">
-                            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F2ndtreasure.1819.joren.bassleer.nxtmediatech.eu%2F&message=I%20donated%20{{$goodiebag->total_kg}}kg%20food%20to%20{{$goodiebag->foodbank->name}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share this to your friends</a>
+                            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F2ndtreasure.live%2F&message=I%20donated%20{{$goodiebag->total_kg}}kg%20food%20to%20{{$goodiebag->foodbank->name}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share this to your friends</a>
                         </div>
+                    </div>
+                    <div class="text-center">
+                        You have helped {{pointToComma($goodiebag->treasures)}} many people
                     </div>
                     @auth
                         
                         <div class="text-center-btn">
-                            <a href="{{route('dashboard.index')}}" class="button2">Check your balance</a>
+                            <a href="{{route('dashboard.index')}}" class="button2">Check your dashboard</a>
                         </div>
                     @endauth
                     @guest
                         <div class="text-center">
-                            <a href="{{route('dashboard.index')}}" class="btn btn-primary">Collect your treasure(s)</a>
+                            <a href="{{route('dashboard.index')}}" class="btn btn-primary">Check your dashboard</a>
                         </div>
                     @endguest
                     <div class="text-center m-3">
